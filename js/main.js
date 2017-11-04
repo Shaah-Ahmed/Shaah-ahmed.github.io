c4b0e3e80d3b97276257f4371e4cdb7410740527
Shaah Ahmed <shaah010@gmail.com>
2017-11-04 22:54:49 +0500

    new website
    not complete

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Page load event handler and scroll position finder enabler 
+
+window.addEventListener("DOMContentLoaded", false);
+
+
+// Function to add class after page load - I love kittypie <3
+window.onload = function animate() {
+
+
+    var x = document.getElementsByClassName("hero");
+    var slideIndex = 0;
+    carousel();
+
+    function carousel() {
+        var i;
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > x.length) {
+            slideIndex = 1
+        }
+        x[slideIndex - 1].style.display = "flex";
+
+        setTimeout(carousel, 7000); // Change Interval
+    };
+
+
+};
