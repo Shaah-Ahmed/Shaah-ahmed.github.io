681c2a97d05bda3a7a39be7aad808f5029bcccb5
Shaah Ahmed <shaah010@gmail.com>
2017-11-15 21:12:46 +0500

    DId the whole background shift

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
-
 // Function to add class after page load - I love kittypie <3
 // window.onload = function animate() {
 
 
-    var x = document.getElementsByClassName("hero");
-    var slideIndex = 0;
-    carousel();
+var x = document.getElementsByClassName("hero");
+var slideIndex = 0;
+carousel();
 
-    function carousel() {
-        var i;
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
-        }
-        slideIndex++;
-        if (slideIndex > x.length) {
-            slideIndex = 1
-        }
-        x[slideIndex - 1].style.display = "flex";
+function carousel() {
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {
+        slideIndex = 1
+    }
+    x[slideIndex - 1].style.display = "flex";
 
-        setTimeout(carousel, 9000); // Change Interval
-    };
+    setTimeout(carousel, 9000); // Change Interval
+};
 
 
 // };
@@ -27,21 +26,23 @@
 
 // ================================================================
 // Waypoints
-// ================================================================
+// =============================
 
-
+// ====================================
+// ============================
+// ==================
 
 
 var waypoint1 = new Waypoint({
     element: document.getElementById('cd'),
     handler: function (direction) {
-        if (direction === "down") {   
+        if (direction === "down") {
             document.getElementById("content-text").classList.add('animated')
-            document.getElementById("content-text").classList.add('fadeInUp')
-            
+            document.getElementById("content-text").classList.add('fadeInRight')
+
             document.getElementById("content-header").classList.add('animated')
-            document.getElementById("content-header").classList.add('fadeInDown')
-            
+            document.getElementById("content-header").classList.add('fadeInLeft')
+
         } else {
             document.getElementById("igloo").classList.remove('animated')
             document.getElementById("igloo").classList.remove('fadeInUp')
@@ -55,29 +56,48 @@ var waypoint2 = new Waypoint({
     handler: function (direction) {
         if (direction === "up") {
             document.getElementById("content-text").classList.remove('animated')
-            document.getElementById("content-text").classList.remove('fadeInUp')
-            
+            document.getElementById("content-text").classList.remove('fadeInRight')
+
             document.getElementById("content-header").classList.remove('animated')
-            document.getElementById("content-header").classList.remove('fadeInDown')
-            
-        } 
+            document.getElementById("content-header").classList.remove('fadeInLeft')
+
+        }
+        
     },
     offset: "90%"
 }, );
 
-var waypoint2 = new Waypoint({
+var waypoint3 = new Waypoint({
     element: document.getElementById('cd'),
     handler: function (direction) {
         if (direction === "down") {
             document.getElementById("igloo").classList.add('animated')
             document.getElementById("igloo").classList.add('fadeInUp')
-        } 
+        }
+        else {
+            document.getElementById("tester").classList.remove('animated')
+            document.getElementById("tester").classList.remove('fadeInUp')  
+        }
     },
     offset: "10%"
+}, );
+
+
+var waypoint4 = new Waypoint({
+    element: document.getElementById('handshake'),
+    handler: function (direction) {
+        if (direction === "down") {
+            document.getElementById("tester").classList.add('animated')
+            document.getElementById("tester").classList.add('fadeInUp')
+        }
+        else {
+            console.log("up") // Use this to remove quote section   
+        }
+    },
+    offset: "20%"
 }, );
 
 
 
 
 // =====================================================================
-
